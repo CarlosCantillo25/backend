7abe1c4932b5ab9a79442fd1944407dc48c8fb76
CarlosCantillo25 <ing.carloscantillo@gmail.com>
2023-06-26 16:38:17 -0500

    minga-07

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import {Router} from "express"
+import  read  from "../controllers/users/read.js"
 
 const auth_router= Router()
 
 // auth_router.post()
-// auth_router.get()
+auth_router.get('/', read)
 // auth_router.put()
 // auth_router.delete()
 
