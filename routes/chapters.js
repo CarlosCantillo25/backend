439ba6bf69a0d587e725c9cecf3e8116278d5ca3
CarlosCantillo25 <ing.carloscantillo@gmail.com>
2023-06-26 16:44:23 -0500

    minga-08

--- routes/chapters.js
+++ routes/chapters.js
@@ -4,7 +4,7 @@ import read from '../controllers/chapters/read.js'
 const chapter_router= Router()
 
 // chapter_router.post()
- chapter_router.get('/',read)
+ chapter_router.get( '/',read)
 // chapter_router.put()
 // chapter_router.delete()
 
